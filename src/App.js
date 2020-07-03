4c9f656ecad3586b14ff5402262e69493e8a6f9b
Rusydy <rusydymuhiddin@gmail.com>
2020-07-03 21:42:22 +0700

    debugging

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-/*
-Let's practice props and mapping components on our todo list app!
-
-I've created a js file with some todos data in it, which I'm imported into this file. (Normally this data would come from an API call, not a local file). 
-
-Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
-*/
-
 import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
 
-function App() {
-    const TextItem = todosData.map(
-        todo => <TodoItem key={todo.id} text={todo.text}/>
-    )
-    
-    // const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
-    return (
-        <div className="todo-list">
-            {TextItem}
-        </div>
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            todos: todosData
+        }
+    }
+
+    render() {
+        // const TextItem = this.state.todos.map(
+        //     todo => <TodoItem todo={{todo: todo.text, completed: todo.completed}} />
+        // )
+        
+        // const TextItem = todosData.map(item => <TodoItem key={item.id} item={{item: item.text}}/>)
+
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+
+        return (
+            <div className="todo-list">
+                {/* {TextItem} */}
+                {todoItems}
+            </div>
+        )
+    }
 }
 
 export default App
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,8 +3,17 @@ import React from "react"
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" /> 
-            <p>{props.text}</p>
+            {/*             
+            {
+                props.todo && (
+                    <div>
+                        <input type="checkbox" checked={props.todo.completed}/>
+                        <p>{props.todo.text}</p>
+                    </div>
+                )
+            } */}
+            <input type="checkbox" checked={props.item.completed}/>
+            <p>{props.item.text}</p>
         </div>
     )
 }
